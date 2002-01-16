e5d2636c1afdba43ba7650af6bff90d4c02cdf61
bh11 <bh11@users.noreply.github.com>
2002-01-16 12:49:42 +0000

    added more operations for socles. Burkhard.

---
+++ lib/socle.gd
@@ -0,0 +1,65 @@
+#############################################################################
+##
+##  socle.gd                         CRISP                 Burkhard H\"ofling
+##
+##  @(#)$Id$
+##
+##  Copyright (C) 2001 by Burkhard H\"ofling, Mathematisches Institut,
+##  Friedrich Schiller-Universit\"at Jena, Germany
+##
+Revision.socle_gd :=
+    "@(#)$Id$";
+
+#############################################################################
+##
+#A  SolvableSocle (<G>) 
+##
+DeclareAttribute ("SolvableSocle", IsGroup);
+DeclareSynonym ("AbelianSocle", SolvableSocle);
+
+#############################################################################
+##
+#A  SocleComponents (<G>) 
+##
+DeclareAttribute ("SocleComponents", IsGroup);
+
+
+#############################################################################
+##
+#A  SolvableSocleComponents (<G>) 
+##
+DeclareAttribute ("SolvableSocleComponents", IsGroup);
+DeclareSynonym ("AbelianSocleComponents", SolvableSocleComponents);
+
+
+#############################################################################
+##
+#A  PSocleComponents (<G>) 
+##
+KeyDependentOperation ("PSocleComponents", IsGroup, IsPosInt, "prime");
+
+
+#############################################################################
+##
+#A  PSocle (<G>) 
+##
+KeyDependentOperation ("PSocle", IsGroup, IsPosInt, "prime");
+
+
+#############################################################################
+##
+#F  SolvableSocleComponentsBySeries (<G>, <ser>) 
+##
+##  G must be a finite group and ser must be a G-composition series of ser[i],
+##  which must be solvable.
+##  SocleComponentsBySeries computes a set [L_1, \ldots, L_r] of minimal
+##  G-invariant subgroups of ser such that Soc(G) \cap ser[1] is the direct
+##  product of the L_i.
+##
+DeclareGlobalFunction ("SolvableSocleComponentsBySeries");
+
+
+#############################################################################
+##
+#E
+##
