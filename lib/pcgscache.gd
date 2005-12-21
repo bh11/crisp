4c855eddaf6ada0d495f8560e7190f787358cc62
bh11 <bh11@users.noreply.github.com>
2005-12-21 16:51:12 +0000

    move optional pcgs cacheing code into separate files. BH

---
+++ lib/pcgscache.gd
@@ -0,0 +1,24 @@
+#############################################################################
+##
+##  pcgscache.gd                    CRISP                 Burkhard H\"ofling
+##
+##  @(#)$Id$
+##
+##  Copyright (C) 2000 by Burkhard H\"ofling, Mathematisches Institut,
+##  Friedrich Schiller-Universit\"at Jena, Germany
+##
+Revision.pcgscache_gd :=
+    "@(#)$Id$";
+
+
+#############################################################################
+##
+#A  InducedPcgsWrtPcgs
+##
+KeyDependentOperation ("InducedPcgsWrtPcgs", IsGroup, IsPcgs, ReturnTrue);
+
+
+#############################################################################
+##
+#E
+##
