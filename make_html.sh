6c4a13274e1ceb3d511fbf8a6eed3b6ce5280b60
bh11 <bh11@users.noreply.github.com>
2011-04-07 07:39:58 +0000

    conversion to bash syntax. BH

--- make_html.sh
+++ make_html.sh
@@ -1,6 +1,7 @@
-rm htm/CHAP???.htm
-rm htm/biblio.htm
-rm htm/theindex.htm
-rm htm/chapters.htm
-perl ../../etc/convert.pl -n CRISP -c -i doc htm
+rm html/CHAP???.html
+rm html/biblio.html
+rm html/theindex.html
+rm html/chapters.html
+perl ../../etc/convert.pl -n CRISP -c -i doc html
+(cd html; for file in *.htm; do mv $file $file"l"; done)
 chmod -R a+r htm
